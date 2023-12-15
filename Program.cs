d95e184a6be8a8be7b7502ded67a986e1da04bc2
Melnyk Mykola <kolanio757@gmail.com>
2023-12-15 21:15:00 +0200

    Mess

--- Program.cs
+++ Program.cs
@@ -13,12 +13,25 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AngularUI",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 // Add ApplicationDbContext and SQL Server support
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration["WorldCitiesAPI:ConnectionStrings:DefaultConnection"])
 );
 
 var app = builder.Build();
+app.UseCors("AngularUI");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
